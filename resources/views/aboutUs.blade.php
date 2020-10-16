c2de1791f9ee9fb395d59021ad0f3802d6c3e7f6
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-10-15 20:21:00 -0400

    cambios

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -8,12 +8,12 @@
         <div class="col-12">
             <h3>Quienes Somos</h3>
         </div>
-        <div class="row">
-            <div class="col-md-5 " style="margin-top: 7rem;">
+        <div class="row" style="margin-top: 7rem;">
+            <div class="col-md-5 ">
                 <img src="{{ $image }}" alt="" style="width: 100%">
             </div>
             
-            <div class="col-md-7" style="margin-top: 1rem;">
+            <div class="col-md-7">
                 {!! $text !!}
             </div>
             <div class="col-md-12">
