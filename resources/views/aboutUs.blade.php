e8321548ff4d42c1702f1ee2b2d78e0e9420830a
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-10-15 20:15:07 -0400

    cambios

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -5,13 +5,19 @@
     @include('partials.navbar')
 
     <div class="container">
+        <div class="col-12">
+            <h3>Quienes Somos</h3>
+        </div>
         <div class="row">
-            <div class="col-md-6 offset-md-3" style="margin-top: 7rem;">
+            <div class="col-md-5 " style="margin-top: 7rem;">
                 <img src="{{ $image }}" alt="" style="width: 100%">
             </div>
             
-            <div class="col-md-10 offset-md-1" style="margin-top: 1rem;">
+            <div class="col-md-7" style="margin-top: 1rem;">
                 {!! $text !!}
+            </div>
+            <div class="col-md-12">
+                {!! $text2 !!}
             </div>
         </div>
     </div>
